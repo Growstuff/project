7971fe5cb9731f3856896fbd38b0d81c0c5deb0c
Miles Gould <miles@assyrian.org.uk>
2012-12-24 14:05:42 +0000

    Script to perform growstuff checkout and setup.
    Based on
    http://wiki.growstuff.org/index.php?title=Development_pre-requisites&oldid=817#Linux_and_OSX

---
+++ script/setup_growstuff.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 [GitHub username]"
+  exit 2;
+fi
+
+curl -L https://get.rvm.io | bash -s stable
+. ~/.rvm/scripts/rvm # uses bashisms
+rvm install 1.9.3-p194
+rvm use 1.9.3-p194
+rvm --default 1.9.3-p194
+# rvm docs generate # not sure we actually need this -MRG
+git clone https://github.com/$1/growstuff.git
+cd growstuff/
+git remote add upstream https://github.com/Growstuff/growstuff.git
+rvm use 1.9.3-p194
+rvm gemset create growstuffdev
+rvm gemset use growstuffdev
+gem install bundler
+git checkout dev
+bundle install
+rake db:create
+rake db:migrate
+rake db:seed
+rake db:test:prepare
+rake
